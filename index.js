3a390733fb940c38a459ecd3f947308c757d7abd
first-name second-name <Samuel2003ngash@gmail.com>
2021-12-31 23:51:56 +0300

    first commit

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var females = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 function verify() {
   var genders = document.getElementsByName("gender");
-  if( year == "" || year.length !=4 || year >2023 || year <=1700) {
+  if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1500) {
      alert( "Insert valid birth year!" );
-     year.focus() ;
+     document.myForm.year.focus() ;
      return false;
   }
-  else if( month.value == "" || isNaN( month.value ) || 
-  month.value.length != 2 || month.value > 12  || month.value <= 0){
-     alert( "Insert valid birth month!" );
-     month.focus() ;
+  else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
+  document.myForm.month.value > 12  || document.myForm.month.value <= 0){
+     alert( "Insert valid birth month! between 1 and 12" );
+     document.myForm.month.focus() ;
      return false;
   }
-  else if( date.value == "" || isNaN( month.value ) || 
-  month.value.length != 2|| date.value > 31 || date.value <= 0) {
+  else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
+  document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
      alert( "Insert valid date!" );
-     date.focus() ;
+     document.myForm.day.focus() ;
      return false;
   }
   else if(genders[0].checked==false && genders[1].checked==false ) {
@@ -56,52 +56,50 @@ function getGender(){
   switch(gender){
     case "male":
       if (dayValue == 1){
-        alert("Hello" +males[0] + " ? You were born on " +days[0]+"!");
+        alert("Hello" +females[0]+ " ? You were born on " +days[0]+"!");
       }
       else if(dayValue == 2){
-        alert("Hello"  +males[1]+ " ? You were born on " +days[1]+"!");
+        alert("Hello" +females[1]+ " ? You were born on " +days[1]+"!");
       }
       else if(dayValue == 3){
-        alert("Hello" +males[2]+ " ? You were born on " +days[2]+"!");
+        alert("Hello" +females[2]+ " ? You were born on " +days[2]+"!");
       }
       else if(dayValue == 4){
-        alert("Hello" +males[3]+ " ? You were born on " +days[3]+"!");
+        alert("Hello" +females[3]+ " ? You were born on " +days[3]+"!");
       }
       else if(dayValue == 5){
-        alert("Hello" +males[4]+ " ? You were born on " +days[4]+"!");
+        alert("Hello" +females[4]+ " ? You were born on " +days[4]+"!");
       }
       else if(dayValue == 6){
-        alert("Hello" +males[5]+ " ? You were born on " +days[5]+"!");
+        alert("Hello" +females[5]+ " ? You were born on " +days[5]+"!");
       }
       else if(dayValue == -0){
-        alert("Hello" +males[6]+ " ? You were born on " +days[6]+"!");
+        alert("Hello" +females[6]+ " ? You were born on " +days[6]+"!");
       }
     break;
     case "female":
       if (dayValue == 1){
-        alert("Hello" +males[0]+ " ? You were born on " +days[0]+"!");
+        alert("Hello" +females[0]+ " ? You were born on " +days[0]+"!");
       }
       else if(dayValue == 2){
-        alert("Hello" +males[1]+ " ? You were born on " +days[1]+"!");
+        alert("Hello" +females[1]+ " ? You were born on " +days[1]+"!");
       }
       else if(dayValue == 3){
-        alert("Hello" +males[2]+ " ? You were born on " +days[2]+"!");
+        alert("Hello" +females[2]+ " ? You were born on " +days[2]+"!");
       }
       else if(dayValue == 4){
-        alert("Hello" +males[3]+ " ? You were born on " +days[3]+"!");
+        alert("Hello" +females[3]+ " ? You were born on " +days[3]+"!");
       }
       else if(dayValue == 5){
-        alert("Hello" +males[4]+ " ? You were born on " +days[4]+"!");
+        alert("Hello" +females[4]+ " ? You were born on " +days[4]+"!");
       }
       else if(dayValue == 6){
-        alert("Hello" +males[4]+ " ? You were born on " +days[4]+"!");
-
+        alert("Hello" +females[5]+ " ? You were born on " +days[5]+"!");
       }
       else if(dayValue == -0){
-        alert("Hello" +males[5]+ " ? You were born on " +days[5]+"!");
+        alert("Hello" +females[6]+ " ? You were born on " +days[6]+"!");
       }
     break
-    default:
           
   }
 }
